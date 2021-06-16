12a571b0123d77b2a51782fb973d2d4ac83a683f
Antonio Rafael Antunes Miranda <36130345+Schwapo@users.noreply.github.com>
2021-06-16 21:36:33 +0200

    Add files via upload

---
+++ ObjectPreviewAttribute/ObjectPreviewAttribute.cs
@@ -0,0 +1,54 @@
+using Sirenix.OdinInspector;
+using System;
+using System.Diagnostics;
+
+[Conditional("UNITY_EDITOR")]
+[AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
+public class ObjectPreviewAttribute : Attribute
+{
+    public float Height { get; set; }
+    public string Tooltip { get; set; }
+    public bool PreviewGetterHasValue { get; private set; }
+    public bool AlignmentHasValue { get; private set; }
+
+    private string previewGetter;
+    private ObjectFieldAlignment alignment;
+
+    public string PreviewGetter
+    {
+        get => previewGetter;
+        set
+        {
+            previewGetter = value;
+            PreviewGetterHasValue = true;
+        }
+    }
+
+    public ObjectFieldAlignment Alignment
+    {
+        get => alignment;
+        set
+        {
+            alignment = value;
+            AlignmentHasValue = true;
+        }
+    }
+
+    public ObjectPreviewAttribute() { }
+
+    public ObjectPreviewAttribute(
+        string previewGetter = "", string tooltip = "")
+        => (PreviewGetter, Tooltip) = (previewGetter, tooltip);
+
+    public ObjectPreviewAttribute(
+        float height, string previewGetter = "", string tooltip = "")
+        => (Height, PreviewGetter, Tooltip) = (height, previewGetter, tooltip);
+
+    public ObjectPreviewAttribute(
+        ObjectFieldAlignment alignment, string previewGetter = "", string tooltip = "")
+        => (Alignment, PreviewGetter, Tooltip) = (alignment, previewGetter, tooltip);
+
+    public ObjectPreviewAttribute(
+        float height, ObjectFieldAlignment alignment, string previewGetter = "", string tooltip = "")
+        => (Height, Alignment, PreviewGetter, Tooltip) = (height, alignment, previewGetter, tooltip);
+}
---
+++ ObjectPreviewAttribute/ObjectPreviewAttributeDrawer.cs
@@ -0,0 +1,125 @@
+#if UNITY_EDITOR
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector.Editor.ValueResolvers;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+public class ObjectPreviewAttributeDrawer<T> : OdinAttributeDrawer<ObjectPreviewAttribute, T>
+    where T : Object
+{
+    private ValueResolver<Texture2D> previewResolver;
+    private bool allowSceneObjects;
+    private float height;
+    private ObjectFieldAlignment alignment;
+
+    protected override void Initialize()
+    {
+        var resolvedString = Attribute.PreviewGetter.IsNullOrWhitespace()
+            ? "@$value.preview"
+            : Attribute.PreviewGetter;
+
+        previewResolver = ValueResolver.Get<Texture2D>(Property, resolvedString);
+        allowSceneObjects = Property.GetAttribute<Sirenix.OdinInspector.AssetsOnlyAttribute>() == null;
+
+        height = Attribute.Height == 0f
+            ? GlobalConfig<GeneralDrawerConfig>.Instance.SquareUnityObjectFieldHeight
+            : Attribute.Height;
+
+        alignment = !Attribute.AlignmentHasValue
+            ? GlobalConfig<GeneralDrawerConfig>.Instance.SquareUnityObjectAlignment
+            : (ObjectFieldAlignment)Attribute.Alignment;
+    }
+
+    protected override void DrawPropertyLayout(GUIContent label)
+    {
+        if (Attribute.PreviewGetterHasValue)
+        {
+            ValueResolver.DrawErrors(previewResolver);
+        }
+
+        var rect = EditorGUILayout.GetControlRect(label != null, height);
+        var dragAndDropId = DragAndDropUtilities.GetDragAndDropId(rect);
+
+        if (label != null)
+        {
+            rect = EditorGUI.PrefixLabel(rect, dragAndDropId, label);
+        }
+
+        rect = alignment switch
+        {
+            ObjectFieldAlignment.Left => rect.AlignLeft(rect.height),
+            ObjectFieldAlignment.Center => rect.AlignCenter(rect.height),
+            _ => rect.AlignRight(rect.height),
+        };
+
+        EditorGUI.LabelField(rect, new GUIContent("", Attribute.Tooltip));
+
+        var value = ValueEntry.SmartValue;
+
+        DrawDropZone(rect, value, null, dragAndDropId);
+
+        value = (T)DragAndDropUtilities.DropZone(rect, value, typeof(T), dragAndDropId);
+
+        value = (T)DragAndDropUtilities.ObjectPickerZone(
+            rect, value, typeof(T), allowSceneObjects, dragAndDropId);
+
+        value = (T)DragAndDropUtilities.DragZone(
+            rect, value, typeof(T), true, true, dragAndDropId);
+
+        if (Event.current.type == EventType.MouseDown && rect.Contains(Event.current.mousePosition))
+        {
+            GUIUtility.keyboardControl = dragAndDropId;
+            GUIUtility.hotControl = dragAndDropId;
+        }
+
+        ValueEntry.SmartValue = value;
+    }
+
+    public void DrawDropZone(Rect rect, object value, GUIContent label, int id)
+    {
+        var isDragging = DragAndDropUtilities.IsDragging;
+
+        if (Event.current.type != EventType.Repaint)
+        {
+            return;
+        }
+
+        var obj = value as Object;
+        var objFieldThumb = EditorStyles.objectFieldThumb;
+
+        var on = GUI.enabled && DragAndDropUtilities.HoveringAcceptedDropZone == id
+            && rect.Contains(Event.current.mousePosition) && isDragging;
+
+        objFieldThumb.Draw(rect, GUIContent.none, id, on);
+
+        if (EditorGUI.showMixedValue)
+        {
+            GUI.Label(rect, "—", SirenixGUIStyles.LabelCentered);
+        }
+        else if (obj)
+        {
+            var texture = previewResolver.GetValue();
+
+            if (texture == null)
+            {
+                texture = GUIHelper.GetAssetThumbnail(
+                    obj, obj.GetType(), preferObjectPreviewOverFileIcon: true);
+            }
+
+            rect = rect.Padding(2f);
+            var rectSize = Mathf.Min(rect.width, rect.height);
+
+            EditorGUI.DrawTextureTransparent(
+                rect.AlignCenter(rectSize, rectSize), texture, ScaleMode.ScaleToFit);
+
+            if (label != null)
+            {
+                rect = rect.AlignBottom(16f);
+                GUI.Label(rect, label, EditorStyles.label);
+            }
+        }
+    }
+}
+#endif
