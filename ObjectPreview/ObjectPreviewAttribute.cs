3d7d1e737505a57776e99994c9b08969502a2c4e
Antonio Rafael Antunes Miranda <36130345+Schwapo@users.noreply.github.com>
2021-09-12 13:13:52 +0200

    Update ObjectPreviewAttribute.cs

--- ObjectPreview/ObjectPreviewAttribute.cs
+++ ObjectPreview/ObjectPreviewAttribute.cs
@@ -1,6 +1,7 @@
 using Sirenix.OdinInspector;
 using System;
 using System.Diagnostics;
+using UnityEngine;
 
 [Conditional("UNITY_EDITOR")]
 [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
